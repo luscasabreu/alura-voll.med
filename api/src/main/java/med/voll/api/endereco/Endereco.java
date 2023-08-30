b916b09f76db3cb62274a2db477c68a3d6a8711b
Lucas Tadeu Abreu de Melo <lucas.melo@appiancapitalbrazil.com>
2023-08-30 00:09:00 -0300

    integrando aplicação com o banco de dados

--- api/src/main/java/med/voll/api/endereco/Endereco.java
+++ api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -2,7 +2,6 @@ package med.voll.api.endereco;
 
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 

29a0013605ed47ff31d31cedaf43b87ee0d780e7
Lucas Tadeu Abreu de Melo <lucas.melo@appiancapitalbrazil.com>
2023-09-12 21:37:43 -0300

    Evoluindo a API com ResponseEntity

---
+++ api/src/main/java/med/voll/api/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,12 @@
+package med.voll.api.paciente;
+
+import med.voll.api.endereco.Endereco;
+import med.voll.api.medico.DadosDetalhamentoMedico;
+
+public record DadosDetalhamentoPaciente(Long id, String nome, String cpf, String email, Endereco endereco) {
+
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+        this(paciente.getId(), paciente.getNome(), paciente.getCpf(), paciente.getEmail(), paciente.getEndereco());
+    }
+
+}

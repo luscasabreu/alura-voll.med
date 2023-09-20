63d0fb7af6ecd19d82206d20340973ca0fbac62b
Lucas Tadeu Abreu de Melo <lucas.melo@appiancapitalbrazil.com>
2023-09-20 20:55:06 -0300

    Começando a implementar camadas de segurança.

---
+++ api/src/main/java/med/voll/api/domain/usuarios/Usuario.java
@@ -0,0 +1,24 @@
+package med.voll.api.domain.usuarios;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name="usuarios")
+@Entity(name="Usuarios")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+
+}
